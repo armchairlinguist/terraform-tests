0da8d311fd318e8ac030f940b7f2d0de6f4368be
Alexis Grant <alexis@hashicorp.com>
2019-02-06 17:25:51 -0800

    remove ec2 for now to examine tf config import

--- ec2-test/main.tf
+++ ec2-test/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
-}
+#resource "aws_instance" "example" {
+#  ami           = "ami-2757f631"
+#  instance_type = "t2.micro"
+#}
