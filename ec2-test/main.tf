4bdd7b475840beff3785806d6ca8053a42eada6e
Alexis Grant <alexis@hashicorp.com>
2019-02-06 17:11:34 -0800

    revert to example ami config

--- ec2-test/main.tf
+++ ec2-test/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region = "us-west-2"
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "${var.ami}"
+  ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
